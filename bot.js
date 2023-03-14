60476316f4506799fc0b52810ad8a0adcf85f91a
sedovnik <66511052+sedovnik@users.noreply.github.com>
2023-03-14 12:27:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,40 +5,36 @@ const GoalBlock = goals.GoalBlock
 const AutoAuth = require('mineflayer-auto-auth')
 const inventoryViewer = require('mineflayer-web-inventory')
 
-const bot = mineflayer.createBot({
+const bot = mineflayer.createBot({ //create and login bot
 
  	host: 'mc.politmine.ru',
 	version: '1.18.2', 
 	auth: 'offline',
 	//host: '192.168.0.103',
 	//port: 62820, 
-	username: 'ironzone20',
+	username: 'Your username',
 	plugins: [AutoAuth],
 	AutoAuth: {
         logging: true,
-        password: '1029384756A',
+        password: 'Your password',
         ignoreRepeat: true,
     }
 
 })
 
-bot.loadPlugin(pathfinder);
-
-
-const mineflayerViewer = require('prismarine-viewer').mineflayer
-inventoryViewer(bot)
+bot.loadPlugin(pathfinder); //to find a way to mobs 
+const mineflayerViewer = require('prismarine-viewer').mineflayer //to watch loaded data
+inventoryViewer(bot) //to check bot's inventory
 
 bot.once('spawn', function () {
-    mineflayerViewer(bot, { firstPerson: false, port: 3007 })
+    mineflayerViewer(bot, { firstPerson: false, port: 3007 }) 
     bot.chat('/joinqueue august')
-
-  
 
 	setInterval(() => {
 		const sword = bot.inventory.items().find(item => item.name.includes('sword'))
-        	if (sword) bot.equip(sword, 'hand')
-		const mobFilter = e => e.type === 'mob' && e.mobType === 'Pig' | e.mobType === 'Cow' | e.mobType === 'Sheep'
-                const mob = bot.nearestEntity(mobFilter)
+        	if (sword) bot.equip(sword, 'hand') //check and equip sword
+		const mobFilter = e => e.type === 'mob' && e.mobType === 'Pig' | e.mobType === 'Cow' | e.mobType === 'Sheep' //filter to search only thease nobs
+                const mob = bot.nearestEntity(mobFilter) //focus on the nearest mob
 
 		if (!mob) {
 			return
@@ -51,22 +47,17 @@ bot.once('spawn', function () {
 		bot.pathfinder.setMovements(movements)
 
 		const goal = new GoalFollow(mob, 1)
-		bot.pathfinder.setGoal(goal, true)
+		bot.pathfinder.setGoal(goal, true) //go to the nearest
 		
 		const pos = mob.position;
-		bot.lookAt(pos) 
-		bot.attack(mob);
+		bot.lookAt(pos) //look at mob
+		bot.attack(mob); //attack mob
       
 
-    }, 500);
+    }, 500); //delay 
 })
 
-bot.on('chat', function (username, message) {
-    if(message === "[main_dismoral -> Я] 1"){
-        bot.chat('(')
-    }
-})
-
+//error logging for simple debug
 bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
 bot.on('error', err => console.log(err))
 
