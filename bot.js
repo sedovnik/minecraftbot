26df4ab1447c0070ff866e5b7887b8e59e93ec7b
sedovnik <66511052+sedovnik@users.noreply.github.com>
2023-03-14 12:41:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,11 +14,12 @@ const bot = mineflayer.createBot({ //create and login bot
 	//port: 62820, 
 	username: 'Your username',
 	plugins: [AutoAuth],
-	AutoAuth: {
-        logging: true,
-        password: 'Your password',
-        ignoreRepeat: true,
-    }
+	AutoAuth: 
+	{
+        	logging: true,
+        	password: 'Your password',
+        	ignoreRepeat: true,
+   	}
 
 })
 
@@ -26,9 +27,9 @@ bot.loadPlugin(pathfinder); //to find a way to mobs
 const mineflayerViewer = require('prismarine-viewer').mineflayer //to watch loaded data
 inventoryViewer(bot) //to check bot's inventory
 
-bot.once('spawn', function () {
-    mineflayerViewer(bot, { firstPerson: false, port: 3007 }) 
-    bot.chat('/joinqueue august')
+bot.once('spawn', function (){
+	mineflayerViewer(bot, { firstPerson: false, port: 3007 }) 
+	bot.chat('/joinqueue august')
 
 	setInterval(() => {
 		const sword = bot.inventory.items().find(item => item.name.includes('sword'))
@@ -36,14 +37,11 @@ bot.once('spawn', function () {
 		const mobFilter = e => e.type === 'mob' && e.mobType === 'Pig' | e.mobType === 'Cow' | e.mobType === 'Sheep' //filter to search only thease nobs
                 const mob = bot.nearestEntity(mobFilter) //focus on the nearest mob
 
-		if (!mob) {
-			return
-		}
+		if (!mob) {return}
 
 		const mcData = require('minecraft-data')(bot.version)
 		const movements = new Movements(bot, mcData)
 		movements.scafoldingBlocks = []
-
 		bot.pathfinder.setMovements(movements)
 
 		const goal = new GoalFollow(mob, 1)
@@ -52,9 +50,8 @@ bot.once('spawn', function () {
 		const pos = mob.position;
 		bot.lookAt(pos) //look at mob
 		bot.attack(mob); //attack mob
-      
-
-    }, 500); //delay 
+		
+		}, 500); //delay 
 })
 
 //error logging for simple debug
